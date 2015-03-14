75bd767cc4baef535a2eab1bc9dcefc150e111cd
Jack Higgins <jackhiggins07@gmail.com>
2015-03-14 17:24:47 +0000

    More restructuring and some work on install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
+BASH_DIR='test/.bash'
 VIM_DIR='test/vim'
 VIMRC='test/vimrc'
+
+# Copy bash stuff
+mkdir -p ${BASH_DIR}
+cp src/bash/* ${BASH_DIR}
+
+# Append src/bashrc to the existing .bashrc or .bas_profile
+
 
 # Install vundle
 mkdir -p ${VIM_DIR}/bundle
