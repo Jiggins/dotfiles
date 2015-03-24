9d7c14276e9b8213e816880b1b0ec701c1c7ffb5
Jack Higgins <jackhiggins07@gmail.com>
2015-03-24 15:30:01 +0000

    Work on train

--- install.sh
+++ install.sh
@@ -1,15 +1,34 @@
 #! /bin/bash
 
+BASHRC='~/.basrc'
 BASH_DIR='test/.bash'
 VIM_DIR='test/vim'
 VIMRC='test/vimrc'
+LOCAL=true
+
+# Mac OSX (Darwin) uses `.bash_profile`, everything else uses `.bashrc`
+if $LOCAL then
+  $BASHRC='./bashrc'
+else
+  case `uname` in
+    'Darwin')
+      $BASHRC='~/.bash_profile'
+      ;;
+    *)
+      $BASHRC='~/.bashrc'
+  esac
+fi
 
 # Copy bash stuff
 mkdir -p ${BASH_DIR}
 cp src/bash/* ${BASH_DIR}
 
-# Append src/bashrc to the existing .bashrc or .bas_profile
-
+# Append src/bashrc to the existing .bashrc or .bash_profile
+if [ -w ${BASHRC} ]
+then
+  cat src/bashrc <(cat ${BASHRC}) > ${BASHRC}
+  source ${BASHRC}
+fi
 
 # Install vundle
 mkdir -p ${VIM_DIR}/bundle
