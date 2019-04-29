37dc0a83c14e1951309b5be989779c797dc71824
Jack higgins <jackhiggins07@gmail.com>
2019-04-29 14:26:51 +0100

    Add Hammerspoon for window layouts
    - `ctrl+alt-cmd 1` - Work with iTem centred
    - `ctrl+alt-cmd 2` - Work with browser centred
    - `ctrl+alt-cmd 3` - Home layout
    
    - `ctrl+alt-cmd w` - Show window information
    - `ctrl+alt-cmd r` - Reload config

---
+++ src/hammerspoon/init.lua
@@ -0,0 +1,86 @@
+-- Reload config
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", hs.reload)
+
+-- Some applications have alternate names which can also be checked if you
+-- enable Spotlight support with
+hs.application.enableSpotlightForNameSearches(true)
+
+hs.layout.mid50       = hs.geometry.new({x = 0.25,  y = 0,     w = 0.5,   h = 1.0})
+hs.layout.top75       = hs.geometry.new({x = 0,     y = 0,     w = 1.0,   h = 0.77})
+hs.layout.bottomleft  = hs.geometry.new({x = 0,     y = 0.77,  w = 0.8,   h = 0.23})
+hs.layout.bottomright = hs.geometry.new({x = 0.8,   y = 0.77,  w = 0.2,   h = 0.23})
+hs.layout.iTunesMini  = hs.geometry.new({x = 0.83,  y = 0,     w = 0.17,  h = 1})
+
+default_browser = "Google Chrome"
+
+-- Layouts
+work = {
+  {"Mail",           nil,             "DELL U3415W",  hs.layout.left25,      nil,  nil},
+  {"iTerm2",         nil,             "DELL U3415W",  hs.layout.mid50,       nil,  nil},
+  {"Amazon Chime",   "Amazon Chime",  "DELL U3415W",  hs.layout.right25,     nil,  nil},
+  {default_browser,  nil,             "DELL U2715H",  hs.layout.maximized,   nil,  nil},
+  {"iTunes",         "Mini Player",   "Color LCD",    hs.layout.iTunesMini,  nil,  nil},
+  {"iTunes",         nil,             "Color LCD",    nil,                   nil,  nil},
+}
+
+work_browser = {
+  {"Mail",           nil,             "DELL U3415W",  hs.layout.left25,      nil,  nil},
+  {default_browser,  nil,             "DELL U3415W",  hs.layout.mid50,       nil,  nil},
+  {"Amazon Chime",   "Amazon Chime",  "DELL U3415W",  hs.layout.right25,     nil,  nil},
+  {"iTerm2",         nil,             "DELL U2715H",  hs.layout.maximized,   nil,  nil},
+  {"iTunes",         "Mini Player",   "Color LCD",    hs.layout.iTunesMini,  nil,  nil},
+  {"iTunes",         nil,             "Color LCD",    nil,                   nil,  nil},
+}
+
+home = {
+  {"Mail",           nil,             "ASUS PB287Q",  hs.layout.left25,      nil,  nil},
+  {"iTerm2",         nil,             "ASUS PB287Q",  hs.layout.mid50,       nil,  nil},
+  {"Amazon Chime",   "Amazon Chime",  "ASUS PB287Q",  hs.layout.right25,     nil,  nil},
+  {default_browser,  nil,             "ASUS PB287Q",  hs.layout.mid50,       nil,  nil},
+  {"iTunes",         "Mini Player",   "Color LCD",    hs.layout.iTunesMini,  nil,  nil},
+  {"iTunes",         nil,             "Color LCD",    nil,                   nil,  nil},
+}
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "E", function()
+  hs.layout.apply(work)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "1", function()
+  hs.layout.apply(work)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "2", function()
+  hs.layout.apply(work_browser)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "3", function()
+  hs.layout.apply(home)
+end)
+
+-- Screens
+-- for i, screen in pairs(hs.screen.allScreens()) do
+--   print(screen:name())
+-- end
+
+function windowdetails()
+  win = hs.window.focusedWindow()
+  frame = win:frame()
+  screen = win:screen()
+
+  output = win:title()
+  output = output .. "\n" .. "Screen name:    " .. screen:name()
+  output = output .. "\n" .. "Top left:       " .. frame.x .. ", " .. frame.y
+  output = output .. "\n" .. "Top left ratio: " .. string.format("%.2f", (frame.x / screen:frame().w)) .. ", " .. string.format("%.2f", (frame.y / screen:frame().h))
+  output = output .. "\n" .. "Bottom right:   " .. frame.x2 .. ", " .. frame.y2
+  output = output .. "\n" .. "Bottom ratio:   " .. string.format("%.2f", (frame.x2 / screen:frame().w)) .. ", " .. string.format("%.2f", (frame.y2 / screen:frame().h))
+  output = output .. "\n" .. "Height:         " .. frame.h
+  output = output .. "\n" .. "Width:          " .. frame.w
+  return output
+end
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  s = windowdetails()
+  print(s)
+  hs.alert.show(s)
+  -- hs.notify.new({title="Hammerspoon", informativeText=s}):send()
+end)
