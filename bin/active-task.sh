e9cbbe438388e4d50c21f47fd87e4807aae61e4c
Jack higgins <jackhiggins07@gmail.com>
2019-07-11 14:27:55 +0100

    Add emoji to the taskwarrior status bar

--- bin/active-task.sh
+++ bin/active-task.sh
@@ -5,6 +5,19 @@ set -eu
 # requires task warrior
 command -v task >/dev/null || exit 1
 
-for id in $(task +ACTIVE _ids | head -n 3); do
-  echo -n " ${id}  $(task _get ${id}.description) "
-done
+task_id=$(task +ACTIVE _ids | head -n 1)
+time=$(timew | sed -n 's/ *Total *//p')
+
+# If there are active tasks, use taskwarrior
+if [[ -n "${task_id}" ]]; then
+  echo -n " ${task_id}  $(task _get ${task_id}.description)  ${time} "
+  exit 0
+fi
+
+task=$(timew | sed -n 's/Tracking "*\([^"]*\)"*$/\1/p')
+
+if [[ -z "${task}" ]]; then
+  exit 1
+fi
+
+echo -n " ${task}  ⌚️ ${time} "
