477ad8749e360c50594474edbc59679a3cbbedcb
Jack Higgins <jackhigg@amazon.com>
2020-05-18 17:14:55 +0000

    Zsh: Make FZF show a preview on CTRL-T

--- src/config/zsh/autoload/fzf.zsh
+++ src/config/zsh/autoload/fzf.zsh
@@ -4,7 +4,9 @@ if command -v fd > /dev/null; then
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_DEFAULT_OPTS="--ansi"
 
-  # if command -v bat > /dev/null; then
-  #   export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --preview '[[ -f {} ]] && bat --style grid --color always {}' --preview-window down:10"
-  # fi
+  if command -v bat > /dev/null; then
+    export FZF_CTRL_T_OPTS="${FZF_DEFAULT_OPTS} --preview '[[ -f {} ]] && bat --style grid --color always {}'"
+  else
+    export FZF_CTRL_T_OPTS="${FZF_DEFAULT_OPTS} --preview '[[ -f {} ]] && cat {}'"
+  fi
 fi
