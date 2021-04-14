490b09e52bf6a5bc3c7d16789daad6b57f1c0f8a
Jack Higgins <jackhigg@amazon.com>
2021-04-14 20:03:50 +0000

    Zsh: Add docker completion

--- src/config/zsh/autoload/docker.zsh
+++ src/config/zsh/autoload/docker.zsh
@@ -1,3 +1,16 @@
+# Completion
+function downloadCompletion() {
+  local completionfile="${HOME}/.config/zsh/completion/_docker"
+
+  if [[ ! -f "${completionfile}" ]]; then
+    curl \
+      -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
+      -o "${completionfile}"
+  fi
+}
+
+downloadCompletion
+
 # Fix Docker trying to use TLS on a unix socket
 if [[ -n "${DOCKER_TLS_VERIFY}" ]] && [[ "${DOCKER_HOST}" =~ 'unix://' ]]; then
   unset DOCKER_TLS_VERIFY
