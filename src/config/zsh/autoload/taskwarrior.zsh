20e9dc1b526c00ecf7ea5fc112f864098c70ddeb
Jack Higgins <jackhiggins07@gmail.com>
2023-05-08 18:00:12 +0100

    ZSH: Add a --start flag to the review function
    This will instantly start a review task, rather than using
    
    ```
    $ review 'the thing' person
    $ task +LATEST start
    ```
    
    just use
    
    ```
    $ review --start 'the thing' person
    ```

--- src/config/zsh/autoload/taskwarrior.zsh
+++ src/config/zsh/autoload/taskwarrior.zsh
@@ -8,6 +8,15 @@ alias n='task ready'
 alias nn="task project:${CURRENT_PROJECT}"
 
 function review() {
+  local start=''
+
+  case "${1}" in
+    --start)
+      start=true
+      shift
+      ;;
+  esac
+
   description="Review ${1}"
   [[ -n "${2}" ]] && description+=" for ${2}"
   shift; shift
@@ -15,5 +24,9 @@ function review() {
   task add scheduled:today ${@} "${description}" \
     || return $?
 
-  task reviews
+  if [[ -n "${start}" ]]; then
+    task +LATEST start
+  else
+    task reviews
+  fi
 }
