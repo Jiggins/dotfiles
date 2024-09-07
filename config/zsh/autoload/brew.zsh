4166b51bd98bcddb866ea2cde71f64ed8ec07228
Jack Higgins <jackhiggins07@gmail.com>
2024-09-07 22:05:24 +0100

    Zsh: Skip homebrew setup if it's not installed

--- config/zsh/autoload/brew.zsh
+++ config/zsh/autoload/brew.zsh
@@ -1,3 +1,3 @@
-if [[ -d "$(brew --prefix)/share/zsh/site-functions" ]]; then
+if command -v brew > /dev/null && [[ -d "$(brew --prefix)/share/zsh/site-functions" ]]; then
   fpath+=( "$(brew --prefix)/share/zsh/site-functions" )
 fi
