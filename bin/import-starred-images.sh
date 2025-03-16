1cf4f7e3eb4f4123e7289f8aceb071c92b1e73e3
Jack Higgins <jackhiggins07@gmail.com>
2025-03-16 12:33:55 +0000

    Create crappy PoC of gif creator script
    This can be done so much better

--- bin/import-starred-images.sh
+++ bin/import-starred-images.sh
@@ -1,13 +1,41 @@
 #¡/usr/bin/env bash
 
-set -eux
+set -eu
 
 declare tmpfile='/tmp/images.json'
 declare export_dir="${EXPORT_DIR:-${HOME}/Desktop/images}"
 mkdir -p "${export_dir}"
 
-find "${@}" -type f -name '*.HIF' -exec exiftool -json -Rating {} + > "${tmpfile}"
+# find "${@}" -type f -name '*.ARW' -exec exiftool -json -Rating {} + > "${tmpfile}"
 
-declare -a images=( $(jq -r '.[] | select(.Rating > 0).SourceFile' "${tmpfile}") )
+# declare -a images=( $(jq -r '.[] | select(.Rating > 0).SourceFile' "${tmpfile}") )
 
-rsync -av "${images[@]}" "${export_dir}"
+# rsync -av "${images[@]}" "${export_dir}"
+
+function makeGif() {
+  local -a images=()
+  local start_recording=false
+
+  while read rating source_file; do
+    if [[ "${rating}" == 2 ]]; then
+      echo "started at ${source_file}"
+      start_recording=true
+    fi
+
+    if [[ "${rating}" == 3 ]]; then
+      echo "Stopped at ${source_file}"
+      images+=( "${source_file}" )
+      break
+    fi
+
+    if [[ "${start_recording}" == true ]]; then
+      images+=( "${source_file}" )
+    fi
+  done < <(jq -r '.[] | [.Rating, .SourceFile] | @tsv' "${tmpfile}")
+  echo "${#images[@]}"
+
+  set -x
+  time magick -verbose "${images[@]}" -delay 5 -loop 0 -resize '20%' "${export_dir}/output.mp4"
+}
+
+makeGif "${@}"
